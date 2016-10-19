88cf5c0d20dfdb3eb3bb79085b8a4c86dfd76ea3
Craig Paul <craig@townfolio.co>
2016-10-19 14:41:26 -0600

    Adds settable functionality through __set

---
+++ src/Settable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CraigPaul\Moneris;
+
+trait Settable
+{
+    /**
+     * Set a property that exists on the class.
+     *
+     * @param string $property
+     *
+     * @throws \InvalidArgumentException
+     * @return void
+     */
+    public function __set(string $property, $value)
+    {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        } else {
+            throw new \InvalidArgumentException('['.get_class($this).'] does not contain a property named ['.$property.']');
+        }
+    }
+}
