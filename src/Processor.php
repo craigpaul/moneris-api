dc72200996eebe0c33e41c579f452d41647bca2c
Craig Paul <craig@townfolio.co>
2016-10-19 14:42:23 -0600

    Updates processor to handle invalid transactions. Adds skeleton for response class

--- src/Processor.php
+++ src/Processor.php
@@ -4,8 +4,24 @@ namespace CraigPaul\Moneris;
 
 class Processor
 {
+    /**
+     * Determine if the request is valid. If so, process the
+     * transaction via the Moneris API.
+     *
+     * @param \CraigPaul\Moneris\Transaction $transaction
+     *
+     * @return \CraigPaul\Moneris\Response
+     */
     public static function process(Transaction $transaction)
     {
-        return new Response();
+        if ($transaction->invalid()) {
+            $response = new Response($transaction);
+            $response->status = Response::INVALID_TRANSACTION_DATA;
+            $response->successful = false;
+
+            return $response;
+        }
+
+        return new Response($transaction);
     }
 }
--- src/Response.php
+++ src/Response.php
@@ -2,7 +2,45 @@
 
 namespace CraigPaul\Moneris;
 
+/**
+ * CraigPaul\Moneris\Response
+ *
+ * @property null|int $status
+ * @property bool $successful
+ * @property \CraigPaul\Moneris\Transaction $transaction
+ */
 class Response
 {
+    use Gettable, Settable;
 
+    const INVALID_TRANSACTION_DATA = 0;
+
+    /**
+     * The status code.
+     *
+     * @var null|int
+     */
+    protected $status = null;
+
+    /**
+     * Determines whether the response was successful.
+     *
+     * @var bool
+     */
+    protected $successful = true;
+
+    /**
+     * @var \CraigPaul\Moneris\Transaction
+     */
+    protected $transaction;
+
+    /**
+     * Create a new Response instance.
+     *
+     * @param \CraigPaul\Moneris\Transaction $transaction
+     */
+    public function __construct(Transaction $transaction)
+    {
+        $this->transaction = $transaction;
+    }
 }
