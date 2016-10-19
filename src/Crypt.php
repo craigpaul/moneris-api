25dbe7620c18153257aa66aaa70b41b4a31c967b
Craig Paul <craig@townfolio.co>
2016-10-19 10:40:33 -0600

    Adds crypt types for purchasing

---
+++ src/Crypt.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CraigPaul\Moneris;
+
+class Crypt
+{
+    const MAIL_TELEPHONE_SINGLE        = 1;
+    const MAIL_TELEPHONE_RECURRING     = 2;
+    const MAIL_TELEPHONE_INSTALLMENT   = 3;
+    const MAIL_TELEPHONE_UNKNOWN       = 4;
+    const AUTHENTICATED_E_COMMERCE     = 5;
+    const NON_AUTHENTICATED_E_COMMERCE = 6;
+    const SSL_ENABLED_MERCHANT         = 7;
+    const NON_SECURE                   = 8;
+    const NON_AUTHENTICATED            = 9;
+}
