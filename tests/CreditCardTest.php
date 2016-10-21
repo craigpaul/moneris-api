cc6a05436411a03abdd68f4efcf9d5eb999bba0e
Craig Paul <craig@townfolio.co>
2016-10-21 11:11:35 -0600

    Adds customer attaching test

--- tests/CreditCardTest.php
+++ tests/CreditCardTest.php
@@ -1,10 +1,28 @@
 <?php
 
 use CraigPaul\Moneris\Crypt;
+use CraigPaul\Moneris\Customer;
 use CraigPaul\Moneris\CreditCard;
 
 class CreditCardTest extends TestCase
 {
+    /**
+     * @var \CraigPaul\Moneris\CreditCard
+     */
+    protected $card;
+
+    /**
+     * Set up the test environment.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->card = CreditCard::create($this->visa, '2012');
+    }
+
     /** @test */
     public function it_can_instantiate_via_the_constructor()
     {
@@ -23,5 +41,15 @@ class CreditCardTest extends TestCase
         $this->assertEquals(CreditCard::class, get_class($card));
         $this->assertObjectHasAttribute('number', $card);
         $this->assertObjectHasAttribute('expiry', $card);
+    }
+
+    /** @test */
+    public function it_can_attach_a_customer_to_a_credit_card()
+    {
+        $customer = Customer::create();
+
+        $this->card->attach($customer);
+
+        $this->assertEquals($customer, $this->card->customer);
     }
 }
