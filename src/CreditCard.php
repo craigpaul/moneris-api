9b4536697a2d349a8ee96468481d6cdd260f36cf
Craig Paul <craig@townfolio.co>
2016-10-21 09:57:03 -0600

    Adds credit card object

---
+++ src/CreditCard.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CraigPaul\Moneris;
+
+/**
+ * CraigPaul\Moneris\CreditCard
+ *
+ * @property-read string $expiry
+ * @property-read string $number
+ */
+class CreditCard
+{
+    use Gettable;
+
+    /**
+     * @var string
+     */
+    protected $number;
+
+    /**
+     * @var string
+     */
+    protected $expiry;
+
+    /**
+     * @var int
+     */
+    protected $crypt;
+
+    /**
+     * Create a new CreditCard instance.
+     *
+     * @param string $number
+     * @param string $expiry
+     * @param int $crypt
+     *
+     * @return void
+     */
+    public function __construct(string $number, string $expiry, int $crypt = 7)
+    {
+        $this->number = $number;
+        $this->expiry = $expiry;
+        $this->crypt = $crypt;
+    }
+
+    /**
+     * Create a new CreditCard instance.
+     *
+     * @param string $number
+     * @param string $expiry
+     * @param int $crypt
+     *
+     * @return $this
+     */
+    public static function create(string $number, string $expiry, int $crypt = 7)
+    {
+        return new static($number, $expiry, $crypt);
+    }
+}
