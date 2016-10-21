7401861c64689a80973e7e72d28ee670650c0bff
Craig Paul <craig@townfolio.co>
2016-10-21 11:48:15 -0600

    Adds set up and add method test

--- tests/VaultTest.php
+++ tests/VaultTest.php
@@ -1,9 +1,27 @@
 <?php
 
 use CraigPaul\Moneris\Vault;
+use CraigPaul\Moneris\CreditCard;
 
 class VaultTest extends TestCase
 {
+    /**
+     * @var \CraigPaul\Moneris\Vault
+     */
+    protected $vault;
+
+    /**
+     * Set up the test environment.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->vault = Vault::create($this->id, $this->token, $this->environment);
+    }
+
     /** @test */
     public function it_can_instantiate_via_the_constructor()
     {
@@ -24,5 +42,17 @@ class VaultTest extends TestCase
         $this->assertObjectHasAttribute('id', $vault);
         $this->assertObjectHasAttribute('token', $vault);
         $this->assertObjectHasAttribute('environment', $vault);
+    }
+
+    /** @test */
+    public function it_can_add_a_credit_card_to_the_moneris_vault_and_returns_a_data_key_for_storage()
+    {
+        $card = CreditCard::create($this->visa, '2012');
+
+        $response = $this->vault->add($card);
+        $receipt = $response->receipt();
+
+        $this->assertTrue($response->successful);
+        $this->assertNotNull($receipt->DataKey);
     }
 }
