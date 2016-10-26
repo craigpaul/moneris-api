c2393778da0353a3893328919362eb080b74170b
Craig Paul <craig@townfolio.co>
2016-10-26 13:26:29 -0600

    Adds logic to get all ResolveData keys when more then one exists in the response

--- src/Preparable.php
+++ src/Preparable.php
@@ -19,36 +19,46 @@ trait Preparable
             $key = $param['key'];
             $property = $param['property'];
 
-            if (is_array($data)) {
-                $array[$property] = isset($data[$key]) && !is_null($data[$key]) ? $data[$key] : null;
-            } else {
-                $array[$property] = isset($data->$key) && !is_null($data->$key) ? $data->$key : null;
-            }
+            if ($key === 'ResolveData' && count($data->xpath('//ResolveData')) > 1) {
+                $resolves = $data->xpath('//ResolveData');
 
-            if (isset($param['cast'])) {
-                switch ($param['cast']) {
-                    case 'boolean':
-                        $array[$property] = isset($array[$property]) ? (is_string($array[$property]) ? $array[$property] : $array[$property]->__toString()) : null;
-                        $array[$property] = isset($array[$property]) && !is_null($array[$property]) ? ($array[$property] === 'true' ? true : false) : false;
-
-                        break;
-                    case 'float':
-                        $array[$property] = isset($array[$property]) ? (is_string($array[$property]) ? floatval($array[$property]) : floatval($array[$property]->__toString())) : null;
-
-                        break;
-                    case 'string':
-                        $array[$property] = isset($array[$property]) ? (is_string($array[$property]) ? $array[$property] : $array[$property]->__toString()) : null;
-
-                        break;
-                    case 'array':
-                        $array[$property] = (array)$array[$property];
+                foreach($resolves as $index => $resolve) {
+                    $resolves[$index] = array_map('strval', (array)$resolve);
                 }
-            }
 
-            if (isset($param['callback'])) {
-                $callback = $param['callback'];
+                $array[$property] = $resolves;
+            } else {
+                if (is_array($data)) {
+                    $array[$property] = isset($data[$key]) && !is_null($data[$key]) ? $data[$key] : null;
+                } else {
+                    $array[$property] = isset($data->$key) && !is_null($data->$key) ? $data->$key : null;
+                }
 
-                $array[$property] = $this->$callback($array[$property]);
+                if (isset($param['cast'])) {
+                    switch ($param['cast']) {
+                        case 'boolean':
+                            $array[$property] = isset($array[$property]) ? (is_string($array[$property]) ? $array[$property] : $array[$property]->__toString()) : null;
+                            $array[$property] = isset($array[$property]) && !is_null($array[$property]) ? ($array[$property] === 'true' ? true : false) : false;
+
+                            break;
+                        case 'float':
+                            $array[$property] = isset($array[$property]) ? (is_string($array[$property]) ? floatval($array[$property]) : floatval($array[$property]->__toString())) : null;
+
+                            break;
+                        case 'string':
+                            $array[$property] = isset($array[$property]) ? (is_string($array[$property]) ? $array[$property] : $array[$property]->__toString()) : null;
+
+                            break;
+                        case 'array':
+                            $array[$property] = (array)$array[$property];
+                    }
+                }
+
+                if (isset($param['callback'])) {
+                    $callback = $param['callback'];
+
+                    $array[$property] = $this->$callback($array[$property]);
+                }
             }
         }
 
