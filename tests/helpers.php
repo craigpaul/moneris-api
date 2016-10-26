ac85c82adf3832d0745c06b461e849ad99c40c33
Craig Paul <craig@townfolio.co>
2016-10-26 13:27:00 -0600

    Adds mock handler helper function

--- tests/helpers.php
+++ tests/helpers.php
@@ -1,5 +1,10 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\MockHandler;
+
 if (!function_exists('dd')) {
     /**
      * @param array ...$args
@@ -9,5 +14,22 @@ if (!function_exists('dd')) {
         call_user_func_array('dump', $args);
 
         die();
+    }
+}
+
+if (!function_exists('mock_handler')) {
+    /**
+     * @param $stub
+     *
+     * @return \GuzzleHttp\Client
+     */
+    function mock_handler($stub)
+    {
+        $mock = new MockHandler([
+            new Response(200, [], $stub)
+        ]);
+        $handler = HandlerStack::create($mock);
+
+        return new Client(['handler' => $handler]);
     }
 }
